8f00e5a7bd7d49a87189f4edec6bed8546d73ae9
Indu-Joshi <indujoshi95@gmail.com>
2023-07-09 20:25:23 +0545

    added animation on scroll

--- script.js
+++ script.js
@@ -4,19 +4,19 @@ const nav = document.querySelector("nav");
 const img12 = document.querySelector(".img12");
 const body = document.querySelector("body");
 
-hamburger.addEventListener("click",()=>{
-    nav.style.display ="block";
-    hamburger.classList.toggle("active");
-    hamburger.style.display="none";
-    closeHamburger.style.display="block";
-    img12.style.display ="none";
-    body.style.backgroundColor = "rgba(0,0,0,.5)";
-})
-closeHamburger.addEventListener("click",()=>{
-    nav.style.display ="none";
-    closeHamburger.classList.toggle("active");
-    hamburger.style.display="block";
-    closeHamburger.style.display="none";
-    img12.style.display ="block";
-    body.style.backgroundColor = "transparent";
-})
+hamburger.addEventListener("click", () => {
+  nav.style.display = "block";
+  hamburger.classList.toggle("active");
+  hamburger.style.display = "none";
+  closeHamburger.style.display = "block";
+  img12.style.display = "none";
+  body.style.backgroundColor = "rgba(0,0,0,.5)";
+});
+closeHamburger.addEventListener("click", () => {
+  nav.style.display = "none";
+  closeHamburger.classList.toggle("active");
+  hamburger.style.display = "block";
+  closeHamburger.style.display = "none";
+  img12.style.display = "block";
+  body.style.backgroundColor = "transparent";
+});
